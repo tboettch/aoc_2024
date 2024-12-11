3509945b117dae0575f534b780e0a3d44e33216d
Tom <tboett@gmail.com>
2024-12-11 14:18:57 -0600

    Solve day 11 part 2

--- aoc_11/src/main.rs
+++ aoc_11/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::{self, prelude::*}};
+use std::{collections::{HashMap}, fs::File, io::{self, prelude::*}};
 
 fn main() -> io::Result<()> {
     // let filename = "example.txt";
@@ -6,15 +6,17 @@ fn main() -> io::Result<()> {
     let filename = "input.txt";
     let mut data = read_data(filename)?;
     // println!("{data:?}");
-    blink_n(&mut data, 25);
-    println!("stone count: {}", data.len());
+    // blink_n(&mut data, 25);
+    // println!("stone count: {}", data.len());
+    // println!("stone25 count: {}", count_blink_all_n(&data, 25));
+    println!("stone75 count: {}", count_blink_all_n(&data, 75));
     Ok(())
 }
 
 fn blink_n(data: &mut Vec<u64>, count: u32) {
     for i in 0..count {
         blink(data);
-        // println!("{data:?}"); // FIXME: Remove
+        // println!("{data:?}");
     }
 }
 
@@ -33,6 +35,38 @@ fn blink(data: &mut Vec<u64>) {
         }
         i += 1;
     }
+}
+
+fn count_blink_all_n(data: &Vec<u64>, count: u32) -> u64 {
+    let mut memo: HashMap<(u64, u32), u64> = HashMap::new();
+    let mut sum: u64 = 0;
+    for stone in data {
+        sum += count_blink_n(*stone, count, &mut memo);
+    }
+    sum
+}
+
+fn count_blink_n(stone: u64, count: u32, memo: &mut HashMap<(u64, u32), u64>) -> u64 {
+    if count == 0 {
+        return 1;
+    }
+    if let Some(r) = memo.get(&(stone, count)) {
+        return *r;
+    }
+    let r = match stone {
+        0 => {
+            count_blink_n(1, count - 1, memo)
+        },
+        x if digits(x) % 2 == 0 => {
+            let (j, k) = split_stone(x);
+            count_blink_n(j, count - 1, memo) + count_blink_n(k, count - 1, memo)
+        },
+        x => {
+            count_blink_n(x * 2024, count - 1, memo)
+        },
+    };
+    memo.insert((stone, count), r);
+    r
 }
 
 fn split_stone(x: u64) -> (u64, u64) {
